83e0734f27772f2ad00b0135d9f28a4e7dcc1526
larryyu2004 <jiayiyu@myyahoo.com>
2025-09-25 15:17:47 +0930

    feat: add tooltip wrapper to display workflow description in WorkflowCard

--- app/(dashboard)/workflows/_components/WorkflowCard.tsx
+++ app/(dashboard)/workflows/_components/WorkflowCard.tsx
@@ -63,11 +63,16 @@ const WorkflowCard = ({ workflow }: { workflow: Workflow }) => {
           </div>
           <div>
             <h3 className="text-base font-bold text-muted-foreground flex items-center">
+
+              <TooltipWrapper content={workflow.description}>
+
+              
               <Link
                 href={`/workflow/editor/${workflow.id}`}
                 className="flex items-center hover:underline">
                 {workflow.name}
               </Link>
+              </TooltipWrapper>
               {isDraft && (
                 <span className="ml-2 px-2 py-0.5 text-xs font-medium text-yellow-800 bg-yellow-100 rounded-full">
                   Draft
