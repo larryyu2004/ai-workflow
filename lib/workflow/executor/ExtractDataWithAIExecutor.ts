694830496ea46e049d7583c906949aaedac8e25f
larryyu2004 <jiayiyu@myyahoo.com>
2025-09-28 21:22:38 +0930

    feat: implement ExtractDataWithAIExecutor using OpenAI chat completions
    - Initialize OpenAI client using credential
    - Run chat completion with content and prompt for data extraction
    - Return extracted JSON data or empty array if no match

--- lib/workflow/executor/ExtractDataWithAIExecutor.ts
+++ lib/workflow/executor/ExtractDataWithAIExecutor.ts
@@ -1,10 +1,8 @@
 import { ExecutionEnvironment } from "@/type/executor";
-import { FillInputTask } from "../task/FillInput";
-import { waitFor } from "@/lib/helper/waitFor";
-import { ClickElementTask } from "../task/ClickElement";
 import { ExtractDataWithAITask } from "../task/ExtractDataWithAI";
 import prisma from "@/lib/prisma";
 import { symmetricDecrypt } from "@/lib/encryption";
+import OpenAI from "openai";
 
 export async function ExtractDataWithAIExecutor(
   environment: ExecutionEnvironment<typeof ExtractDataWithAITask>
@@ -40,12 +38,41 @@ export async function ExtractDataWithAIExecutor(
       return false;
     }
 
-    const mockExtractedData = {
-      usernameSelector: "#username",
-      passwordSelector: "#password",
-      loginSelector: "body > div.container > form > input.btn.btn-primary",
-    };
-    environment.setOutput("Extracted data", JSON.stringify(mockExtractedData));
+    const openai = new OpenAI({
+      apiKey: plainCredentialValue,
+    });
+
+    const response = await openai.chat.completions.create({
+      model: "gpt-4o-mini",
+      messages: [
+        {
+          role: "system",
+          content:
+            "You are a webscraper helper that extracts data from HTML or text. You will be given a piece of text or HTML content as input and also the prompt with the data you have to extract. The response should always be only the extracted data as a JSON array or object, without any additional words or explanations. Analyze the input carefully and extract data precisely based on the prompt. If no data is found, return an empty JSON array. Work only with the provided content and ensure the output is always a valid JSON array without any surrounding text",
+        },
+        {
+          role: "user",
+          content: content,
+        },
+        {
+          role: "user",
+          content: prompt,
+        },
+      ],
+      temperature: 1,
+    });
+    environment.log.info(`Prompt tokens: ${response.usage?.prompt_tokens}`);
+    environment.log.info(
+      `Completition tokens: ${response.usage?.completion_tokens}`
+    );
+
+    const result = response.choices[0].message?.content;
+    if (!result) {
+      environment.log.error("empty response from AI");
+      return false;
+    }
+
+    environment.setOutput("Extracted data", result);
     return true;
   } catch (error: any) {
     environment.log.error(error.message);
