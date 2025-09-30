a51a736fc06a1ad31e1c7bd9194125d8c7da53fd
larryyu2004 <jiayiyu@myyahoo.com>
2025-09-30 13:17:13 +0930

    feat: display task credit cost in TaskMenu button

--- app/workflow/editor/_components/TaskMenu.tsx
+++ app/workflow/editor/_components/TaskMenu.tsx
@@ -6,9 +6,11 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { TaskRegistry } from "@/lib/workflow/task/registry";
 import { TaskType } from "@/type/task";
+import { CoinsIcon } from "lucide-react";
 import React from "react";
 
 export default function TaskMenu() {
@@ -96,6 +98,10 @@ function TaskMenuBtn({ taskType }: { taskType: TaskType }) {
         <task.icon size={20} />
         {task.label}
       </div>
+      <Badge className="gap-2 flex items-center" variant={"outline"}>
+        <CoinsIcon size={16} />
+        {task.credits}
+      </Badge>
     </Button>
   );
 }
