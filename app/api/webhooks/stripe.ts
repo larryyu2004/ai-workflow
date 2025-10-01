a496c9536a628cb1037fa5d1d416ffa7d35a6c8a
larryyu2004 <jiayiyu@myyahoo.com>
2025-10-01 14:54:08 +0930

    fix: changed route.ts name to stripe.ts

--- app/api/webhooks/stripe.ts
+++ app/api/webhooks/stripe.ts
@@ -3,6 +3,7 @@ import { stripe } from "@/lib/stripe/stripe";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 
+
 export async function POST(req: Request) {
   const body = await req.text();
   const signature = headers().get("stripe-signature") as string;
